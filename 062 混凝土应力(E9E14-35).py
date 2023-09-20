7aba7f0d6f7e2bf20ad7deee5d658f64c008e861
Bill <billhu0228@icloud.com>
2023-09-20 20:32:08 +0800

    0920-2

--- 062 混凝土应力(E9E14-35).py
+++ 062 混凝土应力(E9E14-35).py
@@ -1,35 +1,36 @@
+import pandas as pd
+
 from src import GenSection, Superstructure
 from src.bridge import StressInfo
 
 if __name__ == "__main__":
+    pd.set_option('display.max_columns', None)
+    pd.options.display.float_format = '{:,.1f}'.format
     # E13~EA 35米跨中
     no_comp = GenSection(721016.60, 7483.1945, 3.7064E+11, 929.95, 1070.05)
     comp = GenSection(1508516.6, 10813.1945, 9.3682E+11, 1566.8591, 433.1409, 733.1409)
 
+    end_no_comp = GenSection(2036334.755, 6440.8152, 6.8842E+11, 1008.132, 991.868)
+    end_comp = GenSection(2823834.7552, 9770.81, 1.4348E+12, 1326.5717, 673.4283, 973.4283)
+
     gn = 4
     check_loc = 17.5
-    info_pre2 = StressInfo(1860 * 0.75, 42 * 140, -633.0432, -1269.9543)
     info_pre = StressInfo(1860 * 0.75, 38 * 140, -622.8427, -1259.7538)
     info_T1 = StressInfo(1344, 1 * 12 * 140, -609.948, -1246.8591)
     info_T2 = StressInfo(1344, 3 * 12 * 140, -369.948, -1006.8591)
 
-    W9W4 = Superstructure([45, ] * 4 + [35, ], 70.0, 50.0)
-    fcg0 = W9W4.Transfer(girder=gn, loc=check_loc, sec=no_comp, pre_stress=info_pre)
-    fcg0 += W9W4.Erection(girder=gn, loc=check_loc, sec=no_comp, pre_stress=info_pre, fcg=fcg0)
-    fcg0 += W9W4.Deck(girder=gn, loc=check_loc, sec=no_comp, pre_stress=info_pre, fcg=fcg0)
-    dfcg0, fcg_TD1 = W9W4.Post1(girder=gn, loc=check_loc, sec=no_comp, pre_stress=info_pre, T1_stress=info_T1, fcg=fcg0, isNoCP=True)
-    fcg0 += dfcg0
-    dfcg0, dfcg_T1 = W9W4.Deck2(girder=gn, loc=check_loc, sec=no_comp, pre_stress=info_pre, T1_stress=info_T1, fcg=fcg0, fcg_T1=fcg_TD1, isNoCP=True)
-    fcg0 += dfcg0
-    fcg_TD1 += dfcg_T1
-    dfcg0, dfcg_T1, fcg_TD2 = W9W4.Post2(girder=gn, loc=check_loc, sec=comp, pre_stress=info_pre, T1_stress=info_T1, T2_stress=info_T2, fcg=fcg0, fcg_T1=fcg_TD1)
-    fcg0 += dfcg0
-    fcg_TD1 += dfcg_T1
-    dfcg0, dfcg_T1, dfcg_T2 = W9W4.DW(girder=gn, loc=check_loc, sec=comp, pre_stress=info_pre, T1_stress=info_T1, T2_stress=info_T2, fcg=fcg0, fcg_T1=fcg_TD1, fcg_T2=fcg_TD2)
-    fcg0 += dfcg0
-    fcg_TD1 += dfcg_T1
-    fcg_TD2 += dfcg_T2
-    dfcg0, dfcg_T1, dfcg_T2 = W9W4.LongTerm(sec=comp, pre_stress=info_pre, T1_stress=info_T1, T2_stress=info_T2, fcg=fcg0, fcg_T1=fcg_TD1, fcg_T2=fcg_TD2)
-    fcg0 += dfcg0
-    fcg_TD1 += dfcg_T1
-    fcg_TD2 += dfcg_T2
+    end_info_pre = StressInfo(1860 * 0.75, 24 * 140, -586.4653, -904.905)
+    end_info_T1 = StressInfo(829, 1 * 12 * 140, -458.132, -776.5717)
+    end_info_T2 = StressInfo(829, 3 * 12 * 140, 341.868, 23.4283)
+
+    E13EA = Superstructure([45, ] * 4 + [35, ], 70.0, 50.0)
+    print("---------------- 跨中 ----------------")
+    r1 = E13EA.check_stress(girder_n=gn, location=17.5, non_cp_sec=no_comp, cp_section=comp,
+                            pre_stressing=info_pre, post_str_1=info_T1, post_str_2=info_T2,
+                            isMiddle=True)
+    print(r1)
+    print("---------------- 支座 ----------------")
+    r1 = E13EA.check_stress(girder_n=gn, location=34.5, non_cp_sec=end_no_comp, cp_section=end_comp,
+                            pre_stressing=end_info_pre, post_str_1=end_info_T1, post_str_2=end_info_T2,
+                            isMiddle=False)
+    print(r1)
--- src/bridge.py
+++ src/bridge.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 from scipy.optimize import bisect
 import src.common as ss
 import src.creep as cr
@@ -37,6 +39,7 @@ class Superstructure:
         self.day_arr = [7, 180, 183, 183, 186, 186, 190, 10000]
         self.mid_ranges = []
         self.pier_ranges = []
+        self.result = []
         x0 = 0
         for sp in spans:
             x1 = x0 + sp
@@ -44,10 +47,11 @@ class Superstructure:
             self.pier_ranges.append([x0, x0 + (sp - 20) * 0.5])
             self.pier_ranges.append([x1 - (sp - 20) * 0.5, x1])
             x0 = x1
+        self.print_flag = False
         return
 
     def Transfer(self, girder, loc, sec, pre_stress: 'StressInfo', w_g=17.68e6):
-        print("============  放张阶段  ============")
+
         fpc, apc, epc, epc_cp = pre_stress.data
         L = self.spans[girder]
         eff_mat = calculate([L, ], dForce=[0, L], moment_loc=[loc, ])
@@ -57,55 +61,54 @@ class Superstructure:
         fb = sec.pre_stress(-fpc * apc * (1 - ret), epc, -sec.yb) + sec.mom_stress(m_girder, -sec.yb)
         ft = sec.pre_stress(-fpc * apc * (1 - ret), epc, sec.yt) + sec.mom_stress(m_girder, sec.yt)
         f_t_rebar = sec.pre_stress(-fpc * apc * (1 - ret), epc, 949.868) + sec.mom_stress(m_girder, 949.868)
-        print("%.1f | %.1f | %.1f " % (ft, fb, s_fcpg))
-        print("弹性损失 %.1f MPa" % (ret * fpc))
-        print("梁顶钢筋应力 %.1f MPa" % (f_t_rebar / ss.Eci * ss.Es))
+        if self.print_flag:
+            print("============  放张阶段  ============")
+            print("%.1f | %.1f | %.1f " % (ft, fb, s_fcpg))
+            print("弹性损失 %.1f MPa" % (ret * fpc))
+            print("梁顶钢筋应力 %.1f MPa" % (f_t_rebar / ss.Eci * ss.Es))
+        self.result.append([ft, fb, 0])
         return s_fcpg
 
-    def Erection(self, girder, loc, sec, pre_stress: 'StressInfo', fcg, w_g=17.68e6):
-        print("============  架梁阶段  ============")
+    def Erection(self, sec, pre_stress: 'StressInfo', fcg):
         _no, apc, epc, _no = pre_stress.data
-        L = self.spans[girder]
-        eff_mat = calculate([L, ], dForce=[0, L], moment_loc=[loc, ])
-        m_girder = eff_mat['m'][0] * w_g  # Nmm , 简支状态自重跨中弯矩
         dsh, dcr = self.shrink_and_creep(self.day_arr[1], self.day_arr[0], fcg, sec, apc, epc)
         dpe = dsh + dcr
         s_fcpg = sec.pre_stress(dpe * apc, epc, epc)
         fb = sec.pre_stress(dpe * apc, epc, -sec.yb)
         ft = sec.pre_stress(dpe * apc, epc, sec.yt)
-        print("%.1f | %.1f | %.1f " % (ft, fb, s_fcpg))
-        print("收缩徐变损失(先张) %.1f MPa" % (dsh + dcr))
+        if self.print_flag:
+            print("============  架梁阶段  ============")
+            print("%.1f | %.1f | %.1f " % (ft, fb, s_fcpg))
+            print("收缩徐变损失(先张) %.1f MPa" % (dsh + dcr))
+        self.result.append([ft, fb, 0])
         return s_fcpg
 
     def Deck(self, girder, loc, sec, pre_stress: 'StressInfo', fcg, w_deck=26.52 * 1e6):
-        print("============  跨中桥面板  ============")
+        assert sec.ydeck is None
         _no, apc, epc, _no = pre_stress.data
         L = self.spans[girder]
-        range = [x - sum(self.spans[0:girder]) for x in self.mid_ranges[girder]]
-        eff_mat = calculate([L, ], dForce=range, moment_loc=[loc, ])
+        rangex = [x - sum(self.spans[0:girder]) for x in self.mid_ranges[girder]]
+        eff_mat = calculate([L, ], dForce=rangex, moment_loc=[loc, ])
         m_deck = eff_mat['m'][0] * w_deck  # Nmm , 简支状态自重跨中弯矩
-        # m_deck = self.continues(w_deck, self.mid_ranges, sum(self.spans[0:girder]) + loc)
         dsh, dcr = self.shrink_and_creep(self.day_arr[2], self.day_arr[1], fcg, sec, apc, epc)
         dpe = dsh + dcr
         s_fcpg = sec.pre_stress(dpe * apc, epc, epc) + sec.mom_stress(m_deck, epc)
         fb = sec.pre_stress(dpe * apc, epc, -sec.yb) + sec.mom_stress(m_deck, -sec.yb)
         ft = sec.pre_stress(dpe * apc, epc, sec.yt) + sec.mom_stress(m_deck, sec.yt)
-        print("%.1f | %.1f | %.1f " % (ft, fb, s_fcpg))
-        print("收缩徐变损失(先张) %.1f MPa" % (dsh + dcr))
+        if self.print_flag:
+            print("============  跨中桥面板  ============")
+            print("%.1f | %.1f | %.1f " % (ft, fb, s_fcpg))
+            print("收缩徐变损失(先张) %.1f MPa" % (dsh + dcr))
+        self.result.append([ft, fb, 0])
         return s_fcpg
 
-    def Post1(self, girder, loc, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', fcg, isNoCP, w_g=17.68e6, w_deck=26.52 * 1e6):
-        print("============  第1批预应力  ============")
+    def Post1(self, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', fcg, isNoCP):
         if isNoCP:  # 按组合前截面计算
             _no, apc, epc, _no = pre_stress.data
             fpe_T1, apc_T1, epc_T1, _no = T1_stress.data
         else:
             _no, apc, _no, epc = pre_stress.data
             fpe_T1, apc_T1, _no, epc_T1 = T1_stress.data
-        m_deck = self.continues(w_deck, self.mid_ranges, loc)
-        L = self.spans[girder]
-        eff_mat = calculate([L, ], dForce=[0, L], moment_loc=[loc, ])
-        m_girder = eff_mat['m'][0] * w_g  # Nmm , 简支状态自重跨中弯矩
         dsh, dcr = self.shrink_and_creep(self.day_arr[3], self.day_arr[2], fcg, sec, apc, epc)
         desT1 = -sec.pre_stress(-fpe_T1 * apc_T1, epc_T1, epc) / ss.Ec * ss.Ep
         dpe = dsh + dcr + desT1
@@ -117,20 +120,21 @@ class Superstructure:
             fdeck = 0
         else:
             fdeck = sec.pre_stress(dpe * apc, epc, sec.ydeck) + sec.pre_stress(-fpe_T1 * apc_T1, epc_T1, sec.ydeck)
-        print("%.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1))
-        print("弹性损失(先张) %.1f  MPa" % desT1)
-        print("收缩徐变损失(先张) %.1f MPa" % (dsh + dcr))
+        if self.print_flag:
+            print("============  第1批预应力  ============")
+            print("%.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1))
+            print("弹性损失(先张) %.1f  MPa" % desT1)
+            print("收缩徐变损失(先张) %.1f MPa" % (dsh + dcr))
+        self.result.append([ft, fb, fdeck])
         return s_fcpg, s_fcpg_T1
 
-    def Deck2(self, girder, loc, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', fcg, fcg_T1, isNoCP, w_g=17.68e6, w_deck=26.52 * 1e6):
-        print("============ 墩顶桥面板  ============")
+    def Deck2(self, girder, loc, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', fcg, fcg_T1, isNoCP, w_deck=26.52 * 1e6):
         if isNoCP:  # 按组合前截面计算
             _no, apc, epc, _no = pre_stress.data
             fpe_T1, apc_T1, epc_T1, _no = T1_stress.data
         else:
             _no, apc, _no, epc = pre_stress.data
             fpe_T1, apc_T1, _no, epc_T1 = T1_stress.data
-
         m_deck = self.continues(w_deck, self.pier_ranges, sum(self.spans[0:girder]) + loc)
         dsh, dcr = self.shrink_and_creep(self.day_arr[4], self.day_arr[3], fcg, sec, apc, epc)
         dpe = dsh + dcr
@@ -145,21 +149,18 @@ class Superstructure:
             fdeck = 0
         else:
             fdeck = sec.pre_stress(dpe * apc, epc, sec.ydeck) + sec.pre_stress(dpe_T1 * apc_T1, epc_T1, sec.ydeck)
-        print("%.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1))
-        print("收缩徐变损失(先张) %.1f  MPa" % (dsh + dcr))
-        print("收缩徐变损失(后张T1) %.1f  MPa" % (dsh_T1 + dcr_T1))
+        if self.print_flag:
+            print("============ 墩顶桥面板  ============")
+            print("%.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1))
+            print("收缩徐变损失(先张) %.1f  MPa" % (dsh + dcr))
+            print("收缩徐变损失(后张T1) %.1f  MPa" % (dsh_T1 + dcr_T1))
+        self.result.append([ft, fb, fdeck])
         return s_fcpg, s_fcpg_T1
 
-    def Post2(self, girder, loc, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', T2_stress: 'StressInfo',
-              fcg, fcg_T1, w_g=17.68e6, w_deck=26.52 * 1e6):
-        print("============  第2批预应力  ============")
+    def Post2(self, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', T2_stress: 'StressInfo', fcg, fcg_T1):
         _no, apc, _no, epc = pre_stress.data
         fpe_T1, apc_T1, _no, epc_T1 = T1_stress.data
         fpe_T2, apc_T2, _no, epc_T2 = T2_stress.data
-        m_deck = self.continues(w_deck, self.mid_ranges, loc)
-        L = self.spans[girder]
-        eff_mat = calculate([L, ], dForce=[0, L], moment_loc=[loc, ])
-        m_girder = eff_mat['m'][0] * w_g  # Nmm , 简支状态自重跨中弯矩
         dsh, dcr = self.shrink_and_creep(self.day_arr[5], self.day_arr[4], fcg, sec, apc, epc)
         desT2 = -sec.pre_stress(-fpe_T2 * apc_T2, epc_T2, epc) / ss.Ec * ss.Ep
         dpe = dsh + dcr + desT2
@@ -178,16 +179,18 @@ class Superstructure:
         fdeck = (sec.pre_stress(dpe * apc, epc, sec.ydeck) +
                  sec.pre_stress(dpeT1 * apc_T1, epc_T1, sec.ydeck) +
                  sec.pre_stress(-fpe_T2 * apc_T2, epc_T2, sec.ydeck))
-        print("%.1f | %.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1, s_fcpg_T2))
-        print("弹性损失(先张) %.1f  MPa" % desT2)
-        print("弹性损失(第1批后张) %.1f  MPa" % desT2T1)
-        print("收缩徐变损失(先张) %.1f  MPa" % (dsh + dcr))
-        print("收缩徐变损失(后张T1) %.1f  MPa" % (dsh_T1 + dcr_T1))
+        if self.print_flag:
+            print("============  第2批预应力  ============")
+            print("%.1f | %.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1, s_fcpg_T2))
+            print("弹性损失(先张) %.1f  MPa" % desT2)
+            print("弹性损失(第1批后张) %.1f  MPa" % desT2T1)
+            print("收缩徐变损失(先张) %.1f  MPa" % (dsh + dcr))
+            print("收缩徐变损失(后张T1) %.1f  MPa" % (dsh_T1 + dcr_T1))
+        self.result.append([ft, fb, fdeck])
         return s_fcpg, s_fcpg_T1, s_fcpg_T2
 
     def DW(self, girder, loc, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', T2_stress: 'StressInfo',
-           fcg, fcg_T1, fcg_T2, w_g=17.68e6, DW=26.52 * 1e6):
-        print("============ 二期  ============")
+           fcg, fcg_T1, fcg_T2, DW=26.52 * 1e6):
         _no, apc, _no, epc = pre_stress.data
         fpe_T1, apc_T1, _no, epc_T1 = T1_stress.data
         fpe_T2, apc_T2, _no, epc_T2 = T2_stress.data
@@ -214,14 +217,16 @@ class Superstructure:
                  sec.pre_stress(dpe_T2 * apc_T2, epc_T2, sec.ydeck) +
                  sec.mom_stress(m_deck, sec.ydeck)
                  )
-        print("%.1f | %.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1, s_fcpg_T2))
-        print("收缩徐变损失(先张)   %.1f MPa" % (dsh + dcr))
-        print("收缩徐变损失(后张T1) %.1f MPa" % (dsh_T1 + dcr_T1))
-        print("收缩徐变损失(后张T2) %.1f MPa" % (dsh_T2 + dcr_T2))
+        if self.print_flag:
+            print("============ 二期  ============")
+            print("%.1f | %.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1, s_fcpg_T2))
+            print("收缩徐变损失(先张)   %.1f MPa" % (dsh + dcr))
+            print("收缩徐变损失(后张T1) %.1f MPa" % (dsh_T1 + dcr_T1))
+            print("收缩徐变损失(后张T2) %.1f MPa" % (dsh_T2 + dcr_T2))
+        self.result.append([ft, fb, fdeck])
         return s_fcpg, s_fcpg_T1, s_fcpg_T2
 
     def LongTerm(self, sec, pre_stress: 'StressInfo', T1_stress: 'StressInfo', T2_stress: 'StressInfo', fcg, fcg_T1, fcg_T2):
-        print("============ 长期  ============")
         _no, apc, _no, epc = pre_stress.data
         fpe_T1, apc_T1, _no, epc_T1 = T1_stress.data
         fpe_T2, apc_T2, _no, epc_T2 = T2_stress.data
@@ -243,10 +248,13 @@ class Superstructure:
         fdeck = (sec.pre_stress(dpe * apc, epc, sec.ydeck) +
                  sec.pre_stress(dpe_T1 * apc_T1, epc_T1, sec.ydeck) +
                  sec.pre_stress(dpe_T2 * apc_T2, epc_T2, sec.ydeck))
-        print("%.1f | %.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1, s_fcpg_T2))
-        print("收缩徐变损失(先张) %.1f    MPa" % (dsh + dcr))
-        print("收缩徐变损失(后张T1) %.1f  MPa" % (dsh_T1 + dcr_T1))
-        print("收缩徐变损失(后张T2) %.1f  MPa" % (dsh_T2 + dcr_T2))
+        if self.print_flag:
+            print("============ 长期  ============")
+            print("%.1f | %.1f | %.1f | %.1f | %.1f | %.1f" % (ft, fb, fdeck, s_fcpg, s_fcpg_T1, s_fcpg_T2))
+            print("收缩徐变损失(先张) %.1f    MPa" % (dsh + dcr))
+            print("收缩徐变损失(后张T1) %.1f  MPa" % (dsh_T1 + dcr_T1))
+            print("收缩徐变损失(后张T2) %.1f  MPa" % (dsh_T2 + dcr_T2))
+        self.result.append([ft, fb, fdeck])
         return s_fcpg, s_fcpg_T1, s_fcpg_T2
 
     def shrink_and_creep(self, t, t0, sigma0, section: 'GenSection', apc, epc):
@@ -271,3 +279,55 @@ class Superstructure:
         des = ss.Ep / Eci * s_fcpg  # 弹性损失
         loss_check = 1 - (fpc + des) / fpc
         return loss_check - loss_ratio
+
+    def check_stress(self, girder_n, location, non_cp_sec, cp_section, pre_stressing, post_str_1, post_str_2, isMiddle):
+        """
+        混凝土应力核查
+        :param girder_n: 梁编号
+        :param location: 核查位置
+        :param non_cp_sec: 非组合截面
+        :param cp_section: 组合截面
+        :param pre_stressing: 先张
+        :param post_str_1: 第一批后张
+        :param post_str_2: 第二批后张
+        :param isMiddle: 是否是跨中截面
+        :return:
+        """
+        self.result = []
+        post1_sect = cp_section if isMiddle else non_cp_sec
+        deck_sect = cp_section if isMiddle else non_cp_sec
+        fcg0 = self.Transfer(girder=girder_n, loc=location, sec=non_cp_sec, pre_stress=pre_stressing)
+        fcg0 += self.Erection(sec=non_cp_sec, pre_stress=pre_stressing, fcg=fcg0)
+        fcg0 += self.Deck(girder=girder_n, loc=location, sec=non_cp_sec, pre_stress=pre_stressing, fcg=fcg0)
+        dfcg0, fcg_TD1 = self.Post1(sec=post1_sect, pre_stress=pre_stressing, T1_stress=post_str_1, fcg=fcg0, isNoCP=not isMiddle)
+        fcg0 += dfcg0
+        dfcg0, dfcg_T1 = self.Deck2(girder=girder_n, loc=location, sec=deck_sect, pre_stress=pre_stressing, T1_stress=post_str_1, fcg=fcg0, fcg_T1=fcg_TD1, isNoCP=not isMiddle)
+        fcg0 += dfcg0
+        fcg_TD1 += dfcg_T1
+        dfcg0, dfcg_T1, fcg_TD2 = self.Post2(sec=cp_section, pre_stress=pre_stressing, T1_stress=post_str_1, T2_stress=post_str_2, fcg=fcg0, fcg_T1=fcg_TD1)
+        fcg0 += dfcg0
+        fcg_TD1 += dfcg_T1
+        dfcg0, dfcg_T1, dfcg_T2 = self.DW(girder=girder_n, loc=location, sec=cp_section,
+                                          pre_stress=pre_stressing, T1_stress=post_str_1, T2_stress=post_str_2,
+                                          fcg=fcg0, fcg_T1=fcg_TD1, fcg_T2=fcg_TD2)
+        fcg0 += dfcg0
+        fcg_TD1 += dfcg_T1
+        fcg_TD2 += dfcg_T2
+        dfcg0, dfcg_T1, dfcg_T2 = self.LongTerm(sec=cp_section,
+                                                pre_stress=pre_stressing, T1_stress=post_str_1, T2_stress=post_str_2,
+                                                fcg=fcg0, fcg_T1=fcg_TD1, fcg_T2=fcg_TD2)
+        fcg0 += dfcg0
+        fcg_TD1 += dfcg_T1
+        fcg_TD2 += dfcg_T2
+
+        A = [self.result[0], ]
+        for i, item in enumerate(self.result):
+            if i != 0:
+                ft = A[-1][0] + item[0]
+                fb = A[-1][1] + item[1]
+                fdeck = A[-1][2] + item[2]
+                A.append([ft, fb, fdeck])
+        A = pd.DataFrame(A)
+        A = A.T
+        A.columns = ["先张放张", "架梁", "跨中桥面板", "后张-1", "墩顶桥面板", "后张-2", "二期", "运营30Y", ]
+        return A
